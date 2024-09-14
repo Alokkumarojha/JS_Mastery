a16dffad8e8dd2d761042ed24878244e26ad8a56
Alok Kumar Ojha <166001515+Alokkumarojha@users.noreply.github.com>
2024-09-14 11:30:32 +0000

    cmrsn

--- 01_Basics/04_Comparision.js
+++ 01_Basics/04_Comparision.js
@@ -1,5 +1,20 @@
-/console.log(2 > 1);
-console.log(2 < 1);
-console.log(2 >= 1);
-console.log(2 == 1);
-console.log(2 != 1);
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 >= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+const num = 0;
+const obj = new String("0");
+const str = "0";
+
+console.log(num === num); // true
+console.log(obj === obj); // true
+console.log(str === str); // true
+
+console.log(num === obj); // false
+console.log(num === str); // false
+console.log(obj === str); // false
+console.log(null === undefined); // false
+console.log(obj === null); // false
+console.log(obj === undefined); // false
