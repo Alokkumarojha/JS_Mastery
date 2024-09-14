c790869d6b9f909bb45cee6e29c55d35d23286bf
Alok Kumar Ojha <166001515+Alokkumarojha@users.noreply.github.com>
2024-09-14 10:27:48 +0000

    operation

--- 01_Basics/conversionOperation.js
+++ 01_Basics/conversionOperation.js
@@ -34,5 +34,58 @@ let isLoggedIn = "alok" //true
 
 let someNumber = 444;
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber)
+
+//*********************************************Operation*************************************************
+
+
+let value =45;
+let negativeValue = -value
+// console.log(negativeValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*4);
+// console.log(2/2);
+// console.log(2**8); // 2 to the power 2
+// console.log(2%2); // reminder 
+
+// console.log("2"+2+2); //222
+// console.log(2+2+"2"); //42
+// console.log(2+"2"+2); // 222
+// console.log((2+2)+2); // 6 // this is the right way
+
+// console.log(true); // true
+// console.log(+true); // 1 
+// console.log(+""); // 0
+
+
+let str1 = "hello"
+let str2 = " alok"
+let str3 = str1+str2;
+// console.log(str3);  // string can also be added.
+
+let x = 3;
+const y = x++;
+
+// console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+// console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
+
+let gameCounter = 100;
+// gameCounter++; // value of postfixed gets incremented after use
+++gameCounter; // prefixed value is incremented first
+console.log(gameCounter); // 101 
+
+
+
+
+
+
