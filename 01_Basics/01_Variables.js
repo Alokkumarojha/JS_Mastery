3a28768bedeed918fd8424fd54ef01f696928a9f
Alok Kumar Ojha <166001515+Alokkumarojha@users.noreply.github.com>
2024-09-14 01:53:46 +0000

    dataType

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,12 @@
+const accountId = "134435";
+let accoutName = "This is github";
+var emailId = "alok@gmail.com";
+let accountState;
+accountCity = "Siwan"
+
+
+console.table([accountId, accoutName, emailId, accountCity,accountState])
+/*
+Prefer not to use var .
+Becouse of issue in block scope and functional scope.
+*/
---
+++ 01_Basics/02_dataType.js
@@ -0,0 +1,22 @@
+"use Strict"; // Treat all code as a newer version
+
+//alert(4+5) // we are using node js not brouser.
+
+console.log(5 
+    +
+    6)  // Code readability shoud be high.
+
+
+    let name = "Alok";
+    let age = "29";
+    let isLoggedIn = true;
+
+    // Number => 2 to power of 53;
+    // Bigint 
+    // string = ""
+    // boolean  true/fulse
+    // null => standlone value
+    // symboll => use for unique
+    console.table([typeof name])
+    console.log(typeof undefined);
+    console.log(typeof null)  // null is a object.
