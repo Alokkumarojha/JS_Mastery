533ba820b7080247c7e4d615df7657406c95bab0
Alok Kumar Ojha <166001515+Alokkumarojha@users.noreply.github.com>
2024-09-24 09:35:48 +0000

    forOf

---
+++ 05_iterations/03_forOfLoop.js
@@ -0,0 +1,32 @@
+let arr = [1,2,3,4]
+
+for (const val of arr) {
+    // console.log(val);
+    
+} 
+
+let greetings = "hellow world"
+for (const greet of greetings) {
+    if (greet == " ") {
+        // console.log(`space detucted ${greet}`)
+        break
+    }
+    // console.log(`Each char is ${greet}`);
+    
+}
+
+// Map
+
+// map uniwue value ke liye jani jati hai value unique hoti hai jaise ek hi name ke do entry nahi ho sakti. 
+// jis order me value enter hoti hai usi value me print bhi hoti hai.
+
+const Map1 = new Map()
+Map1.set("IN", "India")
+Map1.set("UK", "United kingdom")
+Map1.set("USA", "United state of america")
+// console.log(Map1);
+
+for (const [key , value] of Map1) {
+    console.log(key, ":-" , value);
+    
+}
