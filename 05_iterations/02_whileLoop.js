370f9de929bf1ae8351026791e9e1a6cf9784e8d
Alok Kumar Ojha <166001515+Alokkumarojha@users.noreply.github.com>
2024-09-24 08:35:05 +0000

    dowhileLoop

---
+++ 05_iterations/02_whileLoop.js
@@ -0,0 +1,19 @@
+let index =0
+while (index <=10 ) { 
+    // console.log(`The value of index is ${index}`)
+    index = index + 2
+};
+
+let myHero =  ["Flash","Batman","Spiderman"]
+let arr = 0
+while (arr < myHero.length) {
+    // console.log(`My hero is ${myHero[arr]}`);
+    arr = arr + 1
+}
+
+
+let score = 11
+do {
+    console.log(`The score is ${score}`) // kam pahle 
+    score ++
+} while (score <= 10); // condition check bad  me.
