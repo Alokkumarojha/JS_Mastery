aa46750bda43cbf1595ecaa2df57efe380477c6c
Alok Kumar Ojha <166001515+Alokkumarojha@users.noreply.github.com>
2024-09-25 06:56:30 +0000

    forEach loop

---
+++ 05_iterations/04_forEachLoop.js
@@ -0,0 +1,45 @@
+const coading = ["java","javascript", "c","c++","swift"]
+coading.forEach(function (items) { // without name of fuction is called callbackFN
+    // console.log(items)
+})
+
+const myRoomItems = ["Fan","cloths","Almira","myLaptop","pillow","etc"]
+myRoomItems.forEach( (items) => { // we can use arrow fuction with forEach
+    // console.log(`This is my Room Item ${items}`);
+    
+})
+
+function PrintMe(items) {
+    // console.log(items);
+    
+}
+// myRoomItems.forEach(PrintMe)
+
+myRoomItems.forEach((items,index , arr)=>{
+    // console.log(items)
+})
+
+
+let myCoading = [
+    {
+      languageName: "java script",
+      languageFileName: "js"
+    },
+    {
+        languageName: "Python",
+        languageFileName: "P"
+      },
+      {
+        languageName: "java",
+        languageFileName: "j"
+      },
+      {
+        languageName: "CPP",
+        languageFileName: "c++"
+      },
+]
+myCoading.forEach((items)=>{
+    // console.log(items.languageFileName)
+})
+
+
